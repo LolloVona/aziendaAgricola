40b07c57bbb106d64b2d0cb229332c0136f38445
LolloVona <lorenzo.vona.07@marconigorgonzola.it>
2026-04-16 15:25:59 +0200

    0.0.0
    creazione cartella di lavoro

---
+++ Server/src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+import javax.ws.rs.*;
+import javax.ws.rs.core.*;
+import com.fasterxml.jackson.databind.*;
+public class Main {
+    public static void main(String[] args) {
+        String indirizzoDb="";
+    }
+}
