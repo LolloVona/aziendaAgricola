f4e247177e4a82a7ce75eea6728c69d5e3722fd8
LolloVona <lorenzo.vona.07@marconigorgonzola.it>
2026-05-03 18:33:00 +0200

    .

--- azienda_agricola.sql
+++ azienda_agricola.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Creato il: Apr 25, 2026 alle 14:26
+-- Creato il: Mag 03, 2026 alle 18:31
 -- Versione del server: 10.4.32-MariaDB
 -- Versione PHP: 8.0.30
 
@@ -65,6 +65,14 @@ CREATE TABLE `prodotto` (
   `disponibilita` float NOT NULL
 ) ;
 
+--
+-- Dump dei dati per la tabella `prodotto`
+--
+
+INSERT INTO `prodotto` (`id_prodotto`, `nome`, `prezzo`, `magazzino`, `disponibilita`) VALUES
+(6, 'banana', 10, 54, 23),
+(7, 'pera', 1, 22, 10);
+
 -- --------------------------------------------------------
 
 --
@@ -94,6 +102,34 @@ CREATE TABLE `relativo` (
 -- --------------------------------------------------------
 
 --
+-- Struttura della tabella `spring_session`
+--
+
+CREATE TABLE `spring_session` (
+  `PRIMARY_ID` char(36) NOT NULL,
+  `SESSION_ID` char(36) NOT NULL,
+  `CREATION_TIME` bigint(20) NOT NULL,
+  `LAST_ACCESS_TIME` bigint(20) NOT NULL,
+  `MAX_INACTIVE_INTERVAL` int(11) NOT NULL,
+  `EXPIRY_TIME` bigint(20) NOT NULL,
+  `PRINCIPAL_NAME` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci ROW_FORMAT=DYNAMIC;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `spring_session_attributes`
+--
+
+CREATE TABLE `spring_session_attributes` (
+  `SESSION_PRIMARY_ID` char(36) NOT NULL,
+  `ATTRIBUTE_NAME` varchar(200) NOT NULL,
+  `ATTRIBUTE_BYTES` blob NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci ROW_FORMAT=DYNAMIC;
+
+-- --------------------------------------------------------
+
+--
 -- Struttura della tabella `utente`
 --
 
@@ -104,6 +140,16 @@ CREATE TABLE `utente` (
   `password` varchar(255) NOT NULL,
   `tipo` varchar(1) NOT NULL CHECK (`tipo` in ('C','A'))
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dump dei dati per la tabella `utente`
+--
+
+INSERT INTO `utente` (`id_utente`, `nome`, `username`, `password`, `tipo`) VALUES
+(1, 'Pino', 'Gino', 'prova123', 'A'),
+(2, 'Luca', 'Pino', 'prova', 'C'),
+(3, 'Dino', 'Dino', 'prova', 'C'),
+(4, 'Lino', 'Lino', 'prova', 'C');
 
 --
 -- Indici per le tabelle scaricate
@@ -146,6 +192,21 @@ ALTER TABLE `relativo`
   ADD KEY `id_prodotto` (`id_prodotto`);
 
 --
+-- Indici per le tabelle `spring_session`
+--
+ALTER TABLE `spring_session`
+  ADD PRIMARY KEY (`PRIMARY_ID`),
+  ADD UNIQUE KEY `SPRING_SESSION_IX1` (`SESSION_ID`),
+  ADD KEY `SPRING_SESSION_IX2` (`EXPIRY_TIME`),
+  ADD KEY `SPRING_SESSION_IX3` (`PRINCIPAL_NAME`);
+
+--
+-- Indici per le tabelle `spring_session_attributes`
+--
+ALTER TABLE `spring_session_attributes`
+  ADD PRIMARY KEY (`SESSION_PRIMARY_ID`,`ATTRIBUTE_NAME`);
+
+--
 -- Indici per le tabelle `utente`
 --
 ALTER TABLE `utente`
@@ -155,6 +216,12 @@ ALTER TABLE `utente`
 --
 -- AUTO_INCREMENT per le tabelle scaricate
 --
+
+--
+-- AUTO_INCREMENT per la tabella `acquisto`
+--
+ALTER TABLE `acquisto`
+  MODIFY `numero_fattura` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT per la tabella `aggiornamento`
@@ -178,7 +245,7 @@ ALTER TABLE `prossimoraccolto`
 -- AUTO_INCREMENT per la tabella `utente`
 --
 ALTER TABLE `utente`
-  MODIFY `id_utente` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_utente` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Limiti per le tabelle scaricate
@@ -209,6 +276,12 @@ ALTER TABLE `prossimoraccolto`
 ALTER TABLE `relativo`
   ADD CONSTRAINT `relativo_ibfk_1` FOREIGN KEY (`numero_fattura`) REFERENCES `acquisto` (`numero_fattura`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `relativo_ibfk_2` FOREIGN KEY (`id_prodotto`) REFERENCES `prodotto` (`id_prodotto`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Limiti per la tabella `spring_session_attributes`
+--
+ALTER TABLE `spring_session_attributes`
+  ADD CONSTRAINT `SPRING_SESSION_ATTRIBUTES_FK` FOREIGN KEY (`SESSION_PRIMARY_ID`) REFERENCES `spring_session` (`PRIMARY_ID`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
