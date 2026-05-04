bf29f96cc745b211b337e0d0d3ab38f9db423a84
LolloVona <lorenzo.vona.07@marconigorgonzola.it>
2026-05-04 08:14:38 +0200

    .

--- aziendaAgricolaServer/src/main/java/org/aziendaagricola/controller/ProssimoRaccoltoController.java
+++ aziendaAgricolaServer/src/main/java/org/aziendaagricola/controller/ProssimoRaccoltoController.java
@@ -1,6 +1,7 @@
 package org.aziendaagricola.controller;
 
 import org.aziendaagricola.DTO.ProdottoCreateDTO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,7 +11,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/raccolto")
 public class ProssimoRaccoltoController {
+    @Autowired;
+    private ProssimoRaccoltoService proxRaccoltoService;
     @PostMapping()
+    /*totale:int
+    data:date
+    nomeProdotto:String
+    */
     public ResponseEntity<Object> aggiungiRaccolto(@RequestBody ProdottoCreateDTO dto){
 
     }
