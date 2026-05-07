6bb937d670d73ed76b701bbeee793f690b3511d0
DiegoAlbe <diegoalberghini2007@gmail.com>
2026-05-07 10:31:25 +0200

    v2.6.0
    add registrati();

--- webApp/assets/js/client.js
+++ webApp/assets/js/client.js
@@ -32,3 +32,40 @@ async function accedi(){
                 break;
     }
 }
+
+async function registrati(){
+    var nome = document.getElementById('nome').value;
+    var user = document.getElementById('user').value;
+    var pw = document.getElementById('pass').value;
+
+    const response = await fetch("http://localhost:8080/api/utente/registrazione",
+        {method: "POST",
+            headers:{
+                "Content-type":"application/json"
+            },
+            body: JSON.stringify({
+                username: user,
+                password: pw,
+                nome: nome   
+            })
+
+    })
+
+    //verifico che il server mi abbia risposto:
+    const message = await response.text();
+
+    switch(response.status){
+        case 201:
+                alert('Utente Creato!');
+                break;
+        case 400:
+                alert('errore server Code: 400');
+                break;
+        case 409:
+                alert('Utente già presente');
+                break;
+        default:
+                alert('default case');
+                break;
+    }
+}
